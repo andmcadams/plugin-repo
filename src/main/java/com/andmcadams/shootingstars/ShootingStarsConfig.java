f668d3f4269c2f6303c4e9d9274d113e3c6e89e8
Andrew McAdams <andmcadams@gmail.com>
2021-03-22 23:50:15 -0500

    Add expiration time and clean up config

--- src/main/java/com/andmcadams/shootingstars/ShootingStarsConfig.java
+++ src/main/java/com/andmcadams/shootingstars/ShootingStarsConfig.java
@@ -27,115 +27,248 @@ package com.andmcadams.shootingstars;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
+import net.runelite.client.config.Range;
 
 @ConfigGroup(ShootingStarsPlugin.CONFIG_GROUP_KEY)
-public interface ShootingStarsConfig extends Config {
-    String SHOOTING_STAR_POST_ENDPOINT_KEYNAME = "post endpoint";
-    String SHOOTING_STAR_GET_ENDPOINT_KEYNAME = "get endpoint";
-    String SHOOTING_STAR_SHARED_KEY_KEYNAME = "password";
-    String SHOOTING_STAR_SHOW_PVP_WORLDS_KEYNAME = "pvp worlds";
-    String SHOOTING_STAR_SHOW_ASGARNIA_KEYNAME = "asgarnia";
-    String SHOOTING_STAR_SHOW_KARAMJA_KEYNAME = "karamja";
-    String SHOOTING_STAR_SHOW_FELDIP_HILLS_KEYNAME = "feldip hills";
-    String SHOOTING_STAR_SHOW_FOSSIL_ISLAND_KEYNAME = "fossil island";
-    String SHOOTING_STAR_SHOW_FREMENNIK_KEYNAME = "fremennik";
-    String SHOOTING_STAR_SHOW_KOUREND_KEYNAME = "kourend";
-    String SHOOTING_STAR_SHOW_KANDARIN_KEYNAME = "kandarin";
-    String SHOOTING_STAR_SHOW_KEBOS_KEYNAME = "kebos";
-    String SHOOTING_STAR_SHOW_KHARIDIAN_DESERT_KEYNAME = "desert";
-    String SHOOTING_STAR_SHOW_MISTHALIN_KEYNAME = "misthalin";
-    String SHOOTING_STAR_SHOW_MORYTANIA_KEYNAME = "morytania";
-    String SHOOTING_STAR_SHOW_PISCATORIS_KEYNAME = "piscatoris";
-    String SHOOTING_STAR_SHOW_TIRANNWN_KEYNAME = "tirannwn";
-    String SHOOTING_STAR_SHOW_WILDERNESS_KEYNAME = "wilderness";
+public interface ShootingStarsConfig extends Config
+{
+	String SHOOTING_STAR_POST_ENDPOINT_KEYNAME = "post endpoint";
+	String SHOOTING_STAR_GET_ENDPOINT_KEYNAME = "get endpoint";
+	String SHOOTING_STAR_SHARED_KEY_KEYNAME = "password";
+	String SHOOTING_STAR_EXPIRATION_LENGTH = "expiration";
+	String SHOOTING_STAR_SHOW_PVP_WORLDS_KEYNAME = "pvp worlds";
+	String SHOOTING_STAR_SHOW_ASGARNIA_KEYNAME = "asgarnia";
+	String SHOOTING_STAR_SHOW_KARAMJA_KEYNAME = "karamja";
+	String SHOOTING_STAR_SHOW_FELDIP_HILLS_KEYNAME = "feldip hills";
+	String SHOOTING_STAR_SHOW_FOSSIL_ISLAND_KEYNAME = "fossil island";
+	String SHOOTING_STAR_SHOW_FREMENNIK_KEYNAME = "fremennik";
+	String SHOOTING_STAR_SHOW_KOUREND_KEYNAME = "kourend";
+	String SHOOTING_STAR_SHOW_KANDARIN_KEYNAME = "kandarin";
+	String SHOOTING_STAR_SHOW_KEBOS_KEYNAME = "kebos";
+	String SHOOTING_STAR_SHOW_KHARIDIAN_DESERT_KEYNAME = "desert";
+	String SHOOTING_STAR_SHOW_MISTHALIN_KEYNAME = "misthalin";
+	String SHOOTING_STAR_SHOW_MORYTANIA_KEYNAME = "morytania";
+	String SHOOTING_STAR_SHOW_PISCATORIS_KEYNAME = "piscatoris";
+	String SHOOTING_STAR_SHOW_TIRANNWN_KEYNAME = "tirannwn";
+	String SHOOTING_STAR_SHOW_WILDERNESS_KEYNAME = "wilderness";
 
-    @ConfigItem(keyName = SHOOTING_STAR_POST_ENDPOINT_KEYNAME, position = 0, name = "POST endpoint", description = "Web endpoint to post star data to")
-    default String shootingStarPostEndpointConfig() {
-        return "https://z9smj03u77.execute-api.us-east-1.amazonaws.com/stars";
-    }
+	@ConfigItem(keyName = SHOOTING_STAR_POST_ENDPOINT_KEYNAME, position = 0, name = "POST endpoint", description = "Web endpoint to post star data to")
+	default String shootingStarPostEndpointConfig()
+	{
+		return "https://z9smj03u77.execute-api.us-east-1.amazonaws.com/stars";
+	}
 
-    @ConfigItem(keyName = SHOOTING_STAR_GET_ENDPOINT_KEYNAME, position = 1, name = "GET endpoint", description = "Web endpoint to get star data from")
-    default String shootingStarGetEndpointConfig() {
-        return "https://z9smj03u77.execute-api.us-east-1.amazonaws.com/stars";
-    }
+	@ConfigItem(keyName = SHOOTING_STAR_GET_ENDPOINT_KEYNAME, position = 1, name = "GET endpoint", description = "Web endpoint to get star data from")
+	default String shootingStarGetEndpointConfig()
+	{
+		return "https://z9smj03u77.execute-api.us-east-1.amazonaws.com/stars";
+	}
 
-    @ConfigItem(keyName = SHOOTING_STAR_SHARED_KEY_KEYNAME, position = 2, name = "Key", description = "A keyword to use to share stars with friends. Must be 1-10 alpha characters")
-    default String shootingStarSharedKeyConfig() {
-        return "global";
-    }
+	@ConfigItem(keyName = SHOOTING_STAR_SHARED_KEY_KEYNAME, position = 2, name = "Key", description = "A keyword to use to share stars with friends. Must be 1-10 alpha characters")
+	default String shootingStarSharedKeyConfig()
+	{
+		return "global";
+	}
 
-    @ConfigItem(keyName = SHOOTING_STAR_SHOW_PVP_WORLDS_KEYNAME, position = 3, name = "Show PVP worlds", description = "Show scouted PVP worlds")
-    default boolean shootingStarShowPvpWorlds() {
-        return true;
-    }
+	@ConfigItem(
+		keyName = SHOOTING_STAR_EXPIRATION_LENGTH,
+		position = 2,
+		name = "Time to show landed stars",
+		description = "How many minutes landed stars should remain listed in the side panel"
+	)
+	@Range(
+		max = 10
+	)
+	default int shootingStarExpirationLength()
+	{
+		return 0;
+	}
 
-    @ConfigItem(keyName = SHOOTING_STAR_SHOW_ASGARNIA_KEYNAME, position = 4, name = "Show Asgarnia worlds", description = "Show scouted Asgarnia worlds")
-    default boolean shootingStarShowAsgarniaWorlds() {
-        return true;
-    }
+	@ConfigSection(
+		name = "World Toggles",
+		description = "Settings to show and hide certain types of worlds from the list of scouted worlds",
+		position = 3
+	)
+	String toggleSection = "World Toggles";
 
-    @ConfigItem(keyName = SHOOTING_STAR_SHOW_KARAMJA_KEYNAME, position = 5, name = "Show Karamja worlds", description = "Show scouted Crandor and Karamja worlds")
-    default boolean shootingStarShowKaramjaWorlds() {
-        return true;
-    }
+	@ConfigItem(
+		keyName = SHOOTING_STAR_SHOW_PVP_WORLDS_KEYNAME,
+		position = 3,
+		name = "PVP worlds",
+		description = "Show scouted PVP worlds",
+		section = toggleSection
+	)
+	default boolean shootingStarShowPvpWorlds()
+	{
+		return false;
+	}
 
-    @ConfigItem(keyName = SHOOTING_STAR_SHOW_FELDIP_HILLS_KEYNAME, position = 6, name = "Show Feldip Hills worlds", description = "Show scouted Asgarnia worlds")
-    default boolean shootingStarShowFeldipWorlds() {
-        return true;
-    }
+	@ConfigItem(
+		keyName = SHOOTING_STAR_SHOW_WILDERNESS_KEYNAME,
+		position = 4,
+		name = "Wilderness",
+		description = "Show scouted Wilderness worlds",
+		section = toggleSection
+	)
+	default boolean shootingStarShowWildernessWorlds()
+	{
+		return false;
+	}
 
-    @ConfigItem(keyName = SHOOTING_STAR_SHOW_FOSSIL_ISLAND_KEYNAME, position = 7, name = "Show Fossil Island worlds", description = "Show scouted Fossil Island worlds")
-    default boolean shootingStarShowFossilIslandWorlds() {
-        return true;
-    }
+	@ConfigItem(
+		keyName = SHOOTING_STAR_SHOW_ASGARNIA_KEYNAME,
+		position = 5,
+		name = "Asgarnia",
+		description = "Show scouted Asgarnia worlds",
+		section = toggleSection
+	)
+	default boolean shootingStarShowAsgarniaWorlds()
+	{
+		return true;
+	}
 
-    @ConfigItem(keyName = SHOOTING_STAR_SHOW_FREMENNIK_KEYNAME, position = 8, name = "Show Fremennik worlds", description = "Show scouted Fremennik worlds")
-    default boolean shootingStarShowFremennikWorlds() {
-        return true;
-    }
+	@ConfigItem(
+		keyName = SHOOTING_STAR_SHOW_KARAMJA_KEYNAME,
+		position = 6,
+		name = "Crandor and Karamja",
+		description = "Show scouted Crandor and Karamja worlds",
+		section = toggleSection
+	)
+	default boolean shootingStarShowKaramjaWorlds()
+	{
+		return true;
+	}
 
-    @ConfigItem(keyName = SHOOTING_STAR_SHOW_KOUREND_KEYNAME, position = 9, name = "Show Kourend worlds", description = "Show scouted Kourend worlds")
-    default boolean shootingStarShowKourendWorlds() {
-        return true;
-    }
+	@ConfigItem(
+		keyName = SHOOTING_STAR_SHOW_FELDIP_HILLS_KEYNAME,
+		position = 7,
+		name = "Feldip Hills and Isle of Souls",
+		description = "Show scouted Feldip Hills and Isle of Souls worlds",
+		section = toggleSection
+	)
+	default boolean shootingStarShowFeldipWorlds()
+	{
+		return true;
+	}
 
-    @ConfigItem(keyName = SHOOTING_STAR_SHOW_KANDARIN_KEYNAME, position = 10, name = "Show Kandarin worlds", description = "Show scouted Kandarin worlds")
-    default boolean shootingStarShowKandarinWorlds() {
-        return true;
-    }
+	@ConfigItem(
+		keyName = SHOOTING_STAR_SHOW_FOSSIL_ISLAND_KEYNAME,
+		position = 8,
+		name = "Fossil Island and Mos Le'Harmless",
+		description = "Show scouted Fossil Island and Mos Le'Harmless worlds",
+		section = toggleSection
+	)
+	default boolean shootingStarShowFossilIslandWorlds()
+	{
+		return true;
+	}
 
-    @ConfigItem(keyName = SHOOTING_STAR_SHOW_KEBOS_KEYNAME, position = 11, name = "Show Kebos Lowlands worlds", description = "Show scouted Kebos Lowlands worlds")
-    default boolean shootingStarShowKebosWorlds() {
-        return true;
-    }
+	@ConfigItem(
+		keyName = SHOOTING_STAR_SHOW_FREMENNIK_KEYNAME,
+		position = 9,
+		name = "Fremennik Lands and Lunar Isle",
+		description = "Show scouted Fremennik Lands and Lunar Isle worlds",
+		section = toggleSection
+	)
+	default boolean shootingStarShowFremennikWorlds()
+	{
+		return true;
+	}
 
-    @ConfigItem(keyName = SHOOTING_STAR_SHOW_KHARIDIAN_DESERT_KEYNAME, position = 12, name = "Show Desert worlds", description = "Show scouted Desert worlds")
-    default boolean shootingStarShowDesertWorlds() {
-        return true;
-    }
+	@ConfigItem(
+		keyName = SHOOTING_STAR_SHOW_KOUREND_KEYNAME,
+		position = 10,
+		name = "Great Kourend",
+		description = "Show scouted Great Kourend worlds",
+		section = toggleSection
+	)
+	default boolean shootingStarShowKourendWorlds()
+	{
+		return true;
+	}
 
-    @ConfigItem(keyName = SHOOTING_STAR_SHOW_MISTHALIN_KEYNAME, position = 13, name = "Show Misthalin worlds", description = "Show scouted Misthalin worlds")
-    default boolean shootingStarShowMisthalinWorlds() {
-        return true;
-    }
+	@ConfigItem(
+		keyName = SHOOTING_STAR_SHOW_KANDARIN_KEYNAME,
+		position = 11,
+		name = "Kandarin",
+		description = "Show scouted Kandarin worlds",
+		section = toggleSection
+	)
+	default boolean shootingStarShowKandarinWorlds()
+	{
+		return true;
+	}
 
-    @ConfigItem(keyName = SHOOTING_STAR_SHOW_MORYTANIA_KEYNAME, position = 14, name = "Show Morytania worlds", description = "Show scouted Morytania worlds")
-    default boolean shootingStarShowMorytaniaWorlds() {
-        return true;
-    }
+	@ConfigItem(
+		keyName = SHOOTING_STAR_SHOW_KEBOS_KEYNAME,
+		position = 12,
+		name = "Kebos Lowlands",
+		description = "Show scouted Kebos Lowlands worlds",
+		section = toggleSection
+	)
+	default boolean shootingStarShowKebosWorlds()
+	{
+		return true;
+	}
 
-    @ConfigItem(keyName = SHOOTING_STAR_SHOW_PISCATORIS_KEYNAME, position = 15, name = "Show Piscatoris worlds", description = "Show scouted Piscatoris worlds")
-    default boolean shootingStarShowPiscatorisWorlds() {
-        return true;
-    }
+	@ConfigItem(
+		keyName = SHOOTING_STAR_SHOW_KHARIDIAN_DESERT_KEYNAME,
+		position = 13,
+		name = "Kharidian Desert",
+		description = "Show scouted Kharidian Desert worlds",
+		section = toggleSection
+	)
+	default boolean shootingStarShowDesertWorlds()
+	{
+		return true;
+	}
 
-    @ConfigItem(keyName = SHOOTING_STAR_SHOW_TIRANNWN_KEYNAME, position = 16, name = "Show Tirannwn worlds", description = "Show scouted Tirannwn worlds")
-    default boolean shootingStarShowTirannwnWorlds() {
-        return true;
-    }
+	@ConfigItem(
+		keyName = SHOOTING_STAR_SHOW_MISTHALIN_KEYNAME,
+		position = 14,
+		name = "Misthalin",
+		description = "Show scouted Misthalin worlds",
+		section = toggleSection
+	)
+	default boolean shootingStarShowMisthalinWorlds()
+	{
+		return true;
+	}
 
-    @ConfigItem(keyName = SHOOTING_STAR_SHOW_WILDERNESS_KEYNAME, position = 17, name = "Show Wilderness worlds", description = "Show scouted Wilderness worlds")
-    default boolean shootingStarShowWildernessWorlds() {
-        return true;
-    }
+	@ConfigItem(
+		keyName = SHOOTING_STAR_SHOW_MORYTANIA_KEYNAME,
+		position = 15,
+		name = "Morytania",
+		description = "Show scouted Morytania worlds",
+		section = toggleSection
+	)
+	default boolean shootingStarShowMorytaniaWorlds()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = SHOOTING_STAR_SHOW_PISCATORIS_KEYNAME,
+		position = 16,
+		name = "Piscatoris and the Gnome Stronghold",
+		description = "Show scouted Piscatoris and Gnome Stronghold worlds",
+		section = toggleSection
+	)
+	default boolean shootingStarShowPiscatorisWorlds()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = SHOOTING_STAR_SHOW_TIRANNWN_KEYNAME,
+		position = 17,
+		name = "Tirannwn",
+		description = "Show scouted Tirannwn worlds",
+		section = toggleSection
+	)
+	default boolean shootingStarShowTirannwnWorlds()
+	{
+		return true;
+	}
 }
