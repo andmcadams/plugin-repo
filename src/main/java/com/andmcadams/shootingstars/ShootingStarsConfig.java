b526147977a26468b4fa59a1d0d65106e1d8feee
andmcadams <andmcadams@gmail.com>
2026-01-02 22:10:46 -0600

    Update default url to point to starminers hosted server since they are by far the main users for the telescope side

--- src/main/java/com/andmcadams/shootingstars/ShootingStarsConfig.java
+++ src/main/java/com/andmcadams/shootingstars/ShootingStarsConfig.java
@@ -62,13 +62,13 @@ public interface ShootingStarsConfig extends Config
 	@ConfigItem(keyName = SHOOTING_STAR_POST_ENDPOINT_KEYNAME, position = 0, name = "POST endpoint", description = "Web endpoint to post star data to")
 	default String shootingStarPostEndpointConfig()
 	{
-		return "https://z9smj03u77.execute-api.us-east-1.amazonaws.com/stars";
+		return "https://telescope.starminers.site";
 	}
 
 	@ConfigItem(keyName = SHOOTING_STAR_GET_ENDPOINT_KEYNAME, position = 1, name = "GET endpoint", description = "Web endpoint to get star data from")
 	default String shootingStarGetEndpointConfig()
 	{
-		return "https://z9smj03u77.execute-api.us-east-1.amazonaws.com/stars";
+		return "https://telescope.starminers.site";
 	}
 
 	@ConfigItem(keyName = SHOOTING_STAR_SHARED_KEY_KEYNAME, position = 2, name = "Key", description = "A keyword to use to share stars with friends. Must be 1-10 alpha characters")
